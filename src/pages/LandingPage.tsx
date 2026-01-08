c898bac98b9a87f3957bf8982ec6ac1b5f2fcd42
Sinkó Dávid <sddavid118@gmail.com>
2026-01-08 21:22:02 +0100

    LandingPage design first segment

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -16,15 +16,37 @@ const LandingPage = () => {
 
             <section>
               {/* <button className="border rounded-3xl py-1 px-6 md:px-10"></button> */}
-              <Link to="/login" className="border rounded-3xl py-1 px-6 md:px-10">Login</Link>
+              <Link to="/login" className="bg-primary-green text-white rounded-3xl py-1 px-6 md:px-10">Login</Link>
             </section>
           </div>
         </Navbar>
         
-
+        <main>
+          <div className="relative bg-linear-to-br from-emerald-50 via-white to to-blue-50 py-20 px-15 lg:px-13">
+            <div className="mx-auto max-w-7xl">
+              <div className="">
+                <section className="grid gap-12 lg:gap-8 grid-cols-1 lg:grid-cols-2 items-center">
+                  <article className="flex flex-col gap-8">
+                    <h1 className="text-5xl lg:text-7xl tracking-tight">Complex Nutrition & <span className="text-primary-green">Health Management</span></h1>
+                    <p className="text-2xl text-gray-600 max-w-xl italic font-light tracking-tight">
+                      Everything in one place for a healthier lifestyle.
+                    </p>
+                    <div className="flex flex-col md:flex-row  gap-6 sm:gap-4 text-center">
+                      <Link to="/register" className="bg-primary-green text-white rounded-3xl py-3 px-6 md:px-10 text-lg">Get Started</Link>
+                      <Link to="/login" className="border text-gray-800 rounded-3xl py-3 px-6 md:px-10 text-md italic">Already have an account</Link>
+                    </div>
+                  </article>
+                  <aside className="relative">
+                    <img src="placeholder.svg" alt="" className="size-full"/>
+                  </aside>
+                </section>
+              </div>
+            </div>
+          </div>
+        </main>
         
     </>
   )
 }
 
-export default LandingPage
+export default LandingPage    
